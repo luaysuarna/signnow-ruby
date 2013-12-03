6b01fd65159a5ab36119312377fe09c06308fd74
andresbravog <andresbravog@gmail.com>
2013-12-03 17:42:30 +0100

    Restored request info spec

--- spec/signnow/request/info_spec.rb
+++ spec/signnow/request/info_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 
-describe Paymill::Request::Info do
+describe Signnow::Request::Info do
   describe "#url" do
     it "constructs the url" do
-      info = Paymill::Request::Info.new(:get, "random", OpenStruct.new(id: 1))
+      info = Signnow::Request::Info.new(:get, nil, "random", OpenStruct.new(id: 1))
 
       info.url.should =~ /random/
     end
@@ -11,14 +11,14 @@ describe Paymill::Request::Info do
 
   describe "#path_with_params" do
     it "does nothing when no params" do
-      info = Paymill::Request::Info.new(:get, "random", nil)
+      info = Signnow::Request::Info.new(:get, nil, "random", nil)
       path = "/path/to/someplace"
 
       info.path_with_params(path, {}).should eq path
     end
 
     it "constructs the path with params" do
-      info = Paymill::Request::Info.new(:get, "random", nil)
+      info = Signnow::Request::Info.new(:get, nil, "random", nil)
       path = "/path/to/someplace"
 
       info.path_with_params(path, {random: "stuff"}).should eq "#{path}?random=stuff"
