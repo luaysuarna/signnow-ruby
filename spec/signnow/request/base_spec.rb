765a9567edd64a3495f92b01fde6ac36dc703ff2
andresbravog <andresbravog@gmail.com>
2013-12-03 16:42:47 +0100

    Restored request base spec

--- spec/signnow/request/base_spec.rb
+++ spec/signnow/request/base_spec.rb
@@ -1,27 +1,27 @@
 require "spec_helper"
 
-describe Paymill::Request::Base do
+describe Signnow::Request::Base do
   context "#perform" do
     it "checks for an api key" do
-      Paymill.stub(:api_key).and_return(nil)
+      Signnow.stub(:api_key).and_return(nil)
 
       expect{
-        Paymill::Request::Base.new(nil).perform
-      }.to raise_error Paymill::AuthenticationError
+        Signnow::Request::Base.new(nil).perform
+      }.to raise_error Signnow::AuthenticationError
     end
 
     it "performs an https request" do
-      Paymill.stub(:api_key).and_return("some key")
+      Signnow.stub(:api_key).and_return("some key")
       connection = double
       validator  = double
-      Paymill::Request::Connection.stub(:new).and_return(connection)
-      Paymill::Request::Validator.stub(:new).and_return(validator)
+      Signnow::Request::Connection.stub(:new).and_return(connection)
+      Signnow::Request::Validator.stub(:new).and_return(validator)
 
       connection.should_receive(:setup_https)
       connection.should_receive(:request)
       validator.should_receive(:validated_data_for)
 
-      Paymill::Request::Base.new(nil).perform
+      Signnow::Request::Base.new(nil).perform
     end
   end
 end
